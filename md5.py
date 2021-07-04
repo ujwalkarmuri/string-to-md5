625b05bd4a83def4cb6ae47d4343169a20df5e1e
ujwalkarmuri <86886735+ujwalkarmuri@users.noreply.github.com>
2021-07-04 12:46:46 +0530

    Create md5.py

---
+++ md5.py
@@ -0,0 +1,28 @@
+import hashlib
+import sys
+
+# (C) Ujwalkarumuri
+
+def cmd():
+    line=str(input("enter the line to convert into string \n : "))
+    return (hashup(line))
+
+def boxprint(_string):
+    element = "-"
+    header = (element * len(_string) + "\n")
+    footer = ("\n"+element * len(_string) + "\n")
+    return(header + _string + footer)
+
+def hashup(_string):
+    return (hashlib.md5(_string.encode('utf-8')).hexdigest())
+
+if (len(sys.argv) > 1 ) and type(sys.argv[1] == str):
+    if (sys.argv[1] != "--help" ):
+        print(boxprint(hashup(sys.argv[1])))
+    else:
+        print(sys.argv[0]+":"+"converts string to md5")
+        print()
+        print("Usage:")
+        print("python " + sys.argv[0] + " <string>")
+else:
+    print(boxprint(cmd()))
